46c4d04df3ff6fbba4cda4df8686cc51e1caa277
Seng Keat <corodostudio@gmail.com>
2023-05-07 21:51:20 +0700

    Create stream processing.py

---
+++ stream processing.py
@@ -0,0 +1,31 @@
+# Here's an example Python code for stream processing using the Apache Kafka streaming platform and the Python client library confluent_kafka:
+
+from confluent_kafka import Consumer, KafkaError
+
+c = Consumer({
+    'bootstrap.servers': 'localhost:9092',
+    'group.id': 'mygroup',
+    'auto.offset.reset': 'earliest'
+})
+
+c.subscribe(['mytopic'])
+
+while True:
+    msg = c.poll(1.0)
+
+    if msg is None:
+        continue
+    if msg.error():
+        if msg.error().code() == KafkaError._PARTITION_EOF:
+            print('End of partition reached')
+        else:
+            print('Error occurred: {}'.format(msg.error().str()))
+    else:
+        print('Received message: {}'.format(msg.value().decode('utf-8')))
+
+c.close()
+
+
+# In this example, we create a Kafka consumer that subscribes to the mytopic topic and listens for incoming messages. The poll() method is used to continuously check for new messages, with a timeout of 1 second. When a new message is received, it is printed to the console.
+
+# Note that this is a very basic example and there are many more features and options available with the Kafka streaming platform and the confluent_kafka Python client library.
